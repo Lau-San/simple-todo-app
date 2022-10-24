ce9aa1c56048f1ed031325dfe6aedb064100c96e
Laura Sanchez <lausanch0512@gmail.com>
2022-10-24 12:48:27 -0500

    test: added entries to tasks table in test database

--- api/tests/data.sql
+++ api/tests/data.sql
@@ -2,3 +2,9 @@ INSERT INTO users (username, password)
 VALUES
     ('test', '$2b$10$5ysgXZUJi7MkJWhEhFcZTObGe18G1G.0rnXkewEtXq6ebVx1qpjYW'),
     ('other', '$2b$10$Wdj1lOudt3JXEc6TBI2C6.Wafuv33FRdv9jRd9qtVdPYWmKmbtiTm');
+
+INSERT INTO tasks (user_id, title, is_completed)
+VALUES
+    (1, 'Test task 1', false),
+    (1, 'Test task 2', true),
+    (2, 'Another test task', false);
