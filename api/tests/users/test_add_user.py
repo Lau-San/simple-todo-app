560329568dd30146a26411dbddec5ae34707f0fc
Laura Sanchez <lausanch0512@gmail.com>
2022-10-24 11:44:27 -0500

    Reorganized user tests

--- api/tests/users/test_add_user.py
+++ api/tests/users/test_add_user.py
@@ -3,56 +3,47 @@ from flask.testing import FlaskClient
 from pytodo.db import get_db
 
 
-def test_get_users(client: FlaskClient, app: Flask):
-    response = client.get('/api/users/')
-
-    assert response.status_code == 200
-    assert response.json == [
-        {
-            'id': 1,
-            'username': 'test',
-            'password': '$2b$10$5ysgXZUJi7MkJWhEhFcZTObGe18G1G.0rnXkewEtXq6ebVx1qpjYW'
-        },
-        {
-            'id': 2,
-            'username': 'other',
-            'password': '$2b$10$Wdj1lOudt3JXEc6TBI2C6.Wafuv33FRdv9jRd9qtVdPYWmKmbtiTm'
-        }
-    ]
-
-
-def test_add_user(client: FlaskClient, app: Flask):
-    def search_user_by_username(username: str):
-        with app.app_context():
-            db = get_db()
-            cur = db.cursor()
-            cur.execute(
-                'SELECT * FROM users WHERE username = %s',
-                (username,)
-            )
-            search = cur.fetchall()
-            return search
-
-    # Add a user successfully
+def test_add_user_ok(client: FlaskClient, app: Flask):
     assert client.post(
         '/api/users/new',
         json={'username': 'New User', 'password': 'Some password'}
     ).status_code == 200
-    assert search_user_by_username('New User')
 
-    # Incomplete data
+    with app.app_context():
+        db = get_db()
+        cur = db.cursor()
+        cur.execute(
+            'SELECT * FROM users WHERE username = %s',
+            ('New User',)
+        )
+        search = cur.fetchall()
+        assert search
 
+
+def test_add_user_incomplete_data(client: FlaskClient, app: Flask):
     # No password
+
     response = client.post(
         '/api/users/new',
         json={'username': 'no password'}
     )
     assert response.status_code == 400
+
     if response.json:
         assert 'Not enough' in response.json['message']
-    assert not search_user_by_username('no password')
+
+    with app.app_context():
+        db = get_db()
+        cur = db.cursor()
+        cur.execute(
+            'SELECT * FROM users WHERE username = %s',
+            ('no password',)
+        )
+        search = cur.fetchall()
+        assert not search
 
     # No username
+
     response = client.post(
         '/api/users/new',
         json={'password': 'Don\'t add me'}
@@ -70,11 +61,13 @@ def test_add_user(client: FlaskClient, app: Flask):
         search = cur.fetchall()
         assert not search
 
-    # No data
+
+def test_add_user_no_data(client: FlaskClient, app: Flask):
     response = client.post(
         '/api/users/new',
         json={}
     )
     assert response.status_code == 400
+
     if response.json:
         assert 'No user' in response.json['message']
---
+++ api/tests/users/test_get_users.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask.testing import FlaskClient
+from pytodo.db import get_db
+
+
+def test_get_all_users(client: FlaskClient, app: Flask):
+    response = client.get('/api/users/')
+
+    assert response.status_code == 200
+    assert response.json == [
+        {
+            'id': 1,
+            'username': 'test',
+            'password': '$2b$10$5ysgXZUJi7MkJWhEhFcZTObGe18G1G.0rnXkewEtXq6ebVx1qpjYW'
+        },
+        {
+            'id': 2,
+            'username': 'other',
+            'password': '$2b$10$Wdj1lOudt3JXEc6TBI2C6.Wafuv33FRdv9jRd9qtVdPYWmKmbtiTm'
+        }
+    ]
+
+
+def test_get_user_by_id(client: FlaskClient):
+    response = client.get('/api/users/1')
+    assert response.status_code == 200
+    assert response.json == {
+        'id': 1,
+        'username': 'test',
+        'password': '$2b$10$5ysgXZUJi7MkJWhEhFcZTObGe18G1G.0rnXkewEtXq6ebVx1qpjYW'
+    }
+
+    assert client.get('/api/users/20').status_code == 404
