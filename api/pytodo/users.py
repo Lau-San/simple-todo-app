a7fdd76f2ef88de044c0a4903f38006084756fd9
Laura Sanchez <lausanch0512@gmail.com>
2022-11-01 21:48:46 -0500

    refactor: Cleaned some code

--- api/pytodo/users.py
+++ api/pytodo/users.py
@@ -1,7 +1,7 @@
 from typing import Union, Any
 from flask import Blueprint, jsonify, request, make_response
 from .db import get_db
-from .utils import get_user_data, query_result_to_tasks
+from .utils import get_user_data, query_result_to_tasks, new_error_response
 
 bp = Blueprint('users', __name__, url_prefix='/users')
 
@@ -13,70 +13,55 @@ def get_all_users():
     db = get_db()
     with db.cursor() as cur:
         cur.execute('SELECT * FROM users;')
-        result = cur.fetchall()
+        search = cur.fetchall()
 
-    response = []
-    for _, user_data in enumerate(result):
-        response.append({
+    users = []
+    for _, user_data in enumerate(search):
+        users.append({
             'id': user_data[0],
             'username': user_data[1],
             'password': user_data[2]
         })
 
-    response = jsonify(response)
-    response.status_code = 200
-    return response
+    return jsonify(users)
 
 
 @bp.route('/new', methods=('POST',))
 def add_user():
     """Add a new user to the database."""
 
-    response = make_response()
-
     # Get json data from request
-    data = request.get_json()
+    new_user_data = request.get_json()
 
     # Check that data is not empty
-    if not data:
-        response = jsonify({
-            'message': 'No user data'
-        })
-        response.status_code = 400
-        return response
+    if not new_user_data:
+        return new_error_response(400, 'No user data')
 
     # Check that data has username and password
-    try:
-        username = data['username']
-        password = data['password']
-    except KeyError:
-        response = jsonify({
-            'message': 'Not enough user data',
-        })
-        response.status_code = 400
-        return response
+    if 'username' not in new_user_data:
+        return new_error_response(400, 'No username was provided for new user')
+    if 'password' not in new_user_data:
+        return new_error_response(400, 'No password was provided for new user')
 
     db = get_db()
     with db.cursor() as cur:
 
         # Check if user is already in database
-        cur.execute('SELECT id FROM users WHERE username = %s', (username,))
-        result = cur.fetchall()
-        if result:
-            response = jsonify({
-                'message': 'User already exists',
-            })
-            response.status_code = 400
-            return response
+        cur.execute(
+            'SELECT id FROM users WHERE username = %s',
+            (new_user_data['username'],)
+        )
+        if cur.fetchall():
+            return new_error_response(400, 'User already exists')
 
-        # Add new user
+        # Add user
         cur.execute(
             'INSERT INTO users (username, password) VALUES (%s, %s)',
-            (username, password)
+            (new_user_data['username'], new_user_data['password'])
         )
         db.commit()
 
-    return response
+    return make_response()
 
 
 @bp.route('/<int:user_id>', methods=('GET',))
@@ -84,32 +69,23 @@ def get_user(user_id: int):
     user_exists, user = get_user_data(user_id)
 
     if not user_exists:
-        response = jsonify({
-            'message': "User doesn't exist"
-        })
-        response.status_code = 404
-        return response
+        return new_error_response(404, "User doesn't exist")
 
-    response = jsonify(user)
-    return response
+    return jsonify(user)
 
 
 @bp.route('/<int:user_id>/tasks', methods=('GET',))
 def get_user_tasks(user_id: int):
+    # Check if user exists
     user_exists, _ = get_user_data(user_id)
-
     if not user_exists:
-        response = jsonify({
-            'message': "User doesn't exist"
-        })
-        response.status_code = 404
-        return response
+        return new_error_response(404, "User doesn't exist")
 
+    # Search user's tasks
     db = get_db()
     with db.cursor() as cur:
         cur.execute('SELECT * FROM tasks WHERE user_id = %s', (user_id,))
         search = cur.fetchall()
 
     tasks = query_result_to_tasks(search)
-    response = jsonify(tasks)
-    return response
+    return jsonify(tasks)
